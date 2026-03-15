18d5cea5350b32160eb005ca31b2c300fb56c4d3
Mikylla Cayago <mcacayago@gmail.com>
2026-03-15 23:43:50 +0800

    add: bullet

---
+++ shoot-em-up/bullet.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed: float = 200.0
+@export var owner_group: String
+@onready var destroy_timer : Timer = $DestroyTimer
+
+var move_dir : Vector2
+
+func _process(delta: float) -> void:
+	translate(move_dir * speed * delta)	
+
+# Called when the node enters the scene tree for the first time.
+func _on_body_entered(body):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _on_destroy_timer_timeout():
+	queue_free() # Replace with function body.
