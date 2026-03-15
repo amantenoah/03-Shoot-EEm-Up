4925936b8cf7cbb9b675e7f6f533b7e86197b11f
amantenoah <111347849+amantenoah@users.noreply.github.com>
2026-03-15 20:06:47 +0800

    initial commit + moving background done

---
+++ shoot-em-up/parallax_2d.gd
@@ -0,0 +1,13 @@
+extends Parallax2D
+
+@export var scroll_speed = 100
+@export var level_length = 5750
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scroll_offset.x = max(scroll_offset.x - scroll_speed * delta, -level_length)
