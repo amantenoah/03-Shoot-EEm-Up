b69e98e68bc4ecfcf4f8aaff08b56d044d02b44b
Mikylla Cayago <mcacayago@gmail.com>
2026-03-16 01:27:51 +0800

    add: spawn bullets for player

--- shoot-em-up/player.gd
+++ shoot-em-up/player.gd
@@ -3,12 +3,29 @@ extends CharacterBody2D
 var move_input: Vector2
 const SPEED = 300.0
 
+@export var shoot_rate : float = 0.1
+var last_shoot_time: float 
+
+var bullet_scene: PackedScene = preload("res://bullet.tscn")
+
+@onready var muzzle = $Muzzle
 
 func _physics_process(delta: float) -> void:
 	move_input = Input.get_vector("left","right","up",'down')
 	velocity = move_input * SPEED
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 
 
 	move_and_slide()
+
+func _shoot():
+	last_shoot_time = Time.get_unix_time_from_system()
+	var bullet = bullet_scene.instantiate()
+	get_tree().root.add_child(bullet)
+	bullet.global_position = muzzle.global_position
+				
+func _process(delta):
+	if Input.is_action_pressed("shoot"):
+		if Time.get_unix_time_from_system() - last_shoot_time > shoot_rate:
+			_shoot()
+			
+			
