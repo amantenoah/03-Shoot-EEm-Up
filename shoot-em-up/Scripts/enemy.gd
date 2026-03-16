58d3d96b003f9c227c2ced0112d3085a8974c967
amantenoah <111347849+amantenoah@users.noreply.github.com>
2026-03-17 02:12:15 +0800

    enemies now have hp

--- shoot-em-up/Scripts/enemy.gd
+++ shoot-em-up/Scripts/enemy.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-var speed = 200  # Enemy's own movement speed
+var speed = 100  # Enemy's own movement speed
 @export var parallax_speed_reference: Parallax2D
-@export var explosion_scene: PackedScene  # Exported explosion scene
+const Explosion = preload("res://Scenes/boom.tscn")
+var hp = 2
 
 func _ready():
 	add_to_group("enemy")
@@ -10,21 +11,31 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	var background_speed = 0
 	if parallax_speed_reference:
-		background_speed = -parallax_speed_reference.scroll_speed
+		background_speed = parallax_speed_reference.scroll_speed
 	
 	translate(Vector2.LEFT * (speed + background_speed) * delta)
-
+	
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("projectile") or area.is_in_group("player"):
+		take_damage()
+
+func take_damage():
+	hp -= 1
+	var explosion = Explosion.instantiate()
+	explosion.position = position
+	get_parent().add_child(explosion)
+	if hp <=0:
 		die()
+	
 
 func die():
-	if explosion_scene:
-		var explosion = explosion_scene.instantiate()
-		explosion.global_position = global_position
-		get_parent().add_child(explosion)
-	queue_free()
+	set_process(false)
+	set_physics_process(false)
+	$AnimatedSprite2D.play("explode")
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	print("freed")
+	queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
 	queue_free()
