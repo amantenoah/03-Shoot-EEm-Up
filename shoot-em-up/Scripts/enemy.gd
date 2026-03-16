f9af295a9468734ce0f07a46350f1a7b8dcc2d6a
amantenoah <111347849+amantenoah@users.noreply.github.com>
2026-03-17 02:20:35 +0800

    player can bump into enemies now too

--- shoot-em-up/Scripts/enemy.gd
+++ shoot-em-up/Scripts/enemy.gd
@@ -3,6 +3,7 @@ extends Area2D
 var speed = 100  # Enemy's own movement speed
 @export var parallax_speed_reference: Parallax2D
 const Explosion = preload("res://Scenes/boom.tscn")
+var can_damage = true
 var hp = 2
 
 func _ready():
@@ -16,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	translate(Vector2.LEFT * (speed + background_speed) * delta)
 	
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("projectile") or area.is_in_group("player"):
+	if area.is_in_group("projectile"): 
 		take_damage()
 
 func take_damage():
@@ -39,3 +40,11 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and can_damage:
+		body.take_damage(1)
+		can_damage = false
+		await get_tree().create_timer(0.5).timeout
+		can_damage = true
